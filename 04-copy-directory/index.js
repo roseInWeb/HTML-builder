58604e0feb742c8c218954b3761aa5e1b07675d1
roseInWeb <i.a.semyonova@mail.ru>
2023-03-03 21:59:16 +0400

    fix: task#4 is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const path = require('path');
+const fs = require('fs');
 
+
+const createDir = () => {
+    fs.mkdir(path.resolve(__dirname, './files-copy'), err => {
+        if(err) throw err;
+    });
+}
+
+const removeDir = () => {
+    fs.readdir(path.resolve(__dirname, './files-copy'), (err, arr) => {
+        arr.forEach(el => {
+            fs.unlink(path.resolve(__dirname, `./files-copy/${el}`), err => {
+                if(err) throw err;
+            });
+        })
+    })
+}
+
+const copyDir = () => {
+    fs.readdir(path.resolve(__dirname, './files'), (err, arr) => {
+        arr.forEach(el => {
+            fs.copyFile(path.resolve(__dirname, `./files/${el}`), path.resolve(__dirname, `./files-copy/${el}`), err => {
+                if(err) throw err;
+            });
+        });
+    });
+}
+
+fs.access(path.resolve(__dirname, './files-copy'), (err) => {
+    if (err) {
+        createDir();
+        copyDir();
+    } else {
+        removeDir();
+        copyDir();
+    }
+});
